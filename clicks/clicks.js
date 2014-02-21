15c76806ce5fbe3f362984a760be8957a8da46bf
mkahney <milo.kahney@mac.com>
2014-02-20 18:05:18 -0800

    add

---
+++ clicks/clicks.js
@@ -0,0 +1,8 @@
+/*global $*/
+/*jslint sloppy:true, browser: true*/
+$("h1.page-title").text("itworked!");
+$(window).on('click', function (e) {
+    $('img.logo').css({left: e.clientX,
+                       top: e.clientY});
+    $('img.logo'0.Toggleclass
+    });
